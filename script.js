5f4af958ed41595216e3dcd4c13c5e92ac68d81c
MUNAZIR <60435223+Munazirraza@users.noreply.github.com>
2023-07-27 17:09:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,60 @@
+let promisesArr = [
 
-setTimeout(() => {
-  
-  const table = document.getElementById('my-table');
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Promise 1")
+    }, Math.floor(Math.random * 3000) + 1000)
+  }),
 
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Promise 2")
+    }, Math.floor(Math.random * 3000) + 1000)
+  }),
+
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Promise 3")
+    }, Math.floor(Math.random * 3000) + 1000)
+  }),
+
+]
+
+// console.log(Promise.all(promisesArr));
+
+const tableBody = document.getElementById("output");
+
+
+Promise.all(promisesArr).then((results) => {
+
+  tableBody.textContent = "";
+
+  // Creating the promise name row
+  results.forEach((result) => {
+    const row = document.createElement("tr");
+    const proCol = document.createElement("td");
+    const timeCol = document.createElement("td");
  
-  const promises = [
-    new Promise((resolve) => setTimeout(() => resolve('Promise 1'), Math.floor(Math.random() * 2000) + 1000)),
-    new Promise((resolve) => setTimeout(() => resolve('Promise 2'), Math.floor(Math.random() * 2000) + 1000)),
-    new Promise((resolve) => setTimeout(() => resolve('Promise 3'), Math.floor(Math.random() * 2000) + 1000))
-  ];
+    proCol.textContent = result;
+    timeCol.textContent = (new Date().getTime() - startTime) / 1000;
 
-  // create a row to display the total time taken
-  const totalRow = document.createElement('tr');
-  const totalTitleCell = document.createElement('td');
-  const totalTimeCell = document.createElement('td');
-  totalTitleCell.textContent = 'Total';
-  totalTimeCell.textContent = '0.000';
-  totalRow.appendChild(totalTitleCell);
-  totalRow.appendChild(totalTimeCell);
+    row.appendChild(proCol);
+    row.appendChild(timeCol);
+    tableBody.appendChild(row);
 
-  // add a row to the table to indicate that the data is loading
-  table.appendChild(totalRow);
-  table.insertAdjacentHTML('beforeend', '<tr id="loading"><td colspan="2">Loading...</td></tr>');
+  })
 
-  // wait for all Promises to resolve using Promise.all
-  const startTime = Date.now();
-  Promise.all(promises.map(p => p.then(result => ({ result, time: (Date.now() - startTime) / 1000 })))).then((results) => {
-    // remove the loading row
-    const loadingRow = document.getElementById('loading');
-    if (loadingRow) {
-      loadingRow.parentNode.removeChild(loadingRow);
-    }
+  // Creating the time row for total time taken
 
-    // populate the table with the results
-    results.forEach((result, index) => {
-      const row = document.createElement('tr');
-      const titleCell = document.createElement('td');
-      const timeCell = document.createElement('td');
-      titleCell.textContent = result.result;
-      timeCell.textContent = result.time.toFixed(3);
-      row.appendChild(titleCell);
-      row.appendChild(timeCell);
-      table.appendChild(row);
-    });
+  const timeRow = document.createElement("tr");
+  const timeCol = document.createElement("td");
 
-    // populate the total time row
-    totalTimeCell.textContent = (Date.now() - startTime) / 1000;
-  }).catch((error) => {
-    console.error(error); // handle errors
-  });
-}, 100);
+  const totalDuration = (new Date().getTime()  - startTime) / 1000;
+  timeCol.setAttribute("colspan", "2");
+  timeCol.textContent = `Total: ${totalDuration.toFixed(3)}s`
+
+  timeRow.appendChild(timeCol)
+  tableBody.appendChild(timeRow);
+})
+
+const startTime =  new Date().getTime();
